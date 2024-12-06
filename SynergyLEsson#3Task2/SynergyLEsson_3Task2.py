6609657553991d718e1e7c5b7a51ac46db36e619
maxstar <maksimgavura78@gmail.com>
2024-12-06 17:04:21 +0200

    Добавьте файлы проекта.

---
+++ SynergyLEsson#3Task2/SynergyLEsson_3Task2.py
@@ -0,0 +1,12 @@
+﻿
+stage1 = input("Первый этап эфолюции человека: ")
+stage2 = input("Второй этап эволюции человека: ")
+stage3 = input("Третий этап эволюции человека: ")
+stage4 = input("четветый этап эволюции человека: ")
+stage5 = input("пятый этам эволюции человека: ")
+stage6 = input("Последний этам эволюции человека")
+print(stage1, stage2, stage3, stage4, stage5, stage6, sep=" => ")
+
+# stage1= Pilopithecus, stage2=Australopithecus, stage3= Homo habilis
+# stage4= Homo erectus, stage5= Homo neanderthalensis, stage6= Homo sapiens sapiens
+#  
